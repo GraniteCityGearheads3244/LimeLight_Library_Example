8358100be580369d0ba24a9c8f7c307b8ac4323c
cpapplefamily <cpapplefamily@gmail.com>
2018-12-12 17:35:36 -0600

    Did not get saved with branch. Still need work though

--- src/main/java/oi/limelightvision/limelight/frc/LimeLight.java
+++ src/main/java/oi/limelightvision/limelight/frc/LimeLight.java
@@ -3,6 +3,7 @@ package oi.limelightvision.limelight.frc;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import oi.limelightvision.limelight.frc.ControlMode.*;
@@ -57,6 +58,7 @@ public class LimeLight {
 
     public boolean isConnected(){
         resetPilelineLatency();
+        Timer.delay(.05);  //How to make this not hold the thread?
         if(getPipelineLatency()==0.0){
             return false;
         }else{
